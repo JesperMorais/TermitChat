6d7a83e6542fcc1eccbcf69cf01020ba34c2dc84
JesperMorais <jeppe.morris@hotmail.se>
2024-12-04 09:34:40 +0100

    fixed client bug

--- client/src/client_main.cpp
+++ client/src/client_main.cpp
@@ -25,13 +25,13 @@ void client_task() {
     MQTTClient_create(&client, ADDRESS, CLIENTID, 0 , NULL);
     MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
 
-    cout << "trying to connect to MQTT" << endl;
+    std::cout << "trying to connect to MQTT" << endl;
     if((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS) {
         printf("Failed to connect, return code %d\n", rc);
         exit(-1);
     }
     else {
-        cout << "CONNECTED TO MQTT BROKER!!!" << endl;
+        std::cout << "CONNECTED TO MQTT BROKER!!!" << endl;
     }
 
     MQTTClient_subscribe(client, TOPIC, QOS);
@@ -47,7 +47,7 @@ int main() {
 
     // Relaunch the program in a new terminal if not already in one
     if (getenv("IN_NEW_TERMINAL") == nullptr) {
-        std::string command = "start cmd.exe /k \"set IN_NEW_TERMINAL=1 && build\\server\\debug\\server\"";
+        std::string command = "start cmd.exe /k \"set IN_NEW_TERMINAL=1 && build\\client\\debug\\client\"";
         std::system(command.c_str());
         return 0;
     }
@@ -57,16 +57,36 @@ int main() {
     string input_content_client_name;
     string clientName;
 
-    auto client_input = Input(&input_content_client_name, )
-    auto client_input = Button("Fortsätt", [&] {
+    auto client_input = Input(&input_content_client_name, "skriv namn...");
+    auto client_button = Button("Fortsätt..", [&] {
         if(!input_content_client_name.empty()){
             input_content_client_name = true;
             clientName = input_content_client_name;
+        }else{
+            cout << "no empty username allowed" << endl;
         }
-    })
+    }) | color(Color::Blue3Bis) | bold | border;
 
+    auto first_container = Container::Vertical({
+        client_input,
+        client_button,
+    });
+
+    auto start_renderer = Renderer(first_container, [&] {
+        return vbox({
+            text("Välkommen till Termit!") | bold | center | color(Color::Blue),
+            separator(),
+            text("Ange client namn för att fortsätta:"),
+            client_input->Render() | border,
+            client_button->Render(),
+            separator()
+        });
+    });
+
+    screen.Loop(first_container);
+    //JAHAOP
     cout << "Starting client..." << endl;
-    client_task(); //Starta thread istället
+    //client_task(); //Starta thread istället
 
     return 0;
 }
